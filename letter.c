cddcbdfa8e692d112394b016866e6ad1aac65501
SubashreeB <subabakth07@gmail.com>
2016-06-06 14:16:43 +0530

    Create letter.c

---
+++ letter.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  char a;
+  scanf("%c",&a);
+  int b=a;
+  if(a>=65&&a<=122)
+  {
+     printf("the character is an alphabet");
+  }
+  else
+  {
+     printf("Its a number");
+  }
+}
+   
